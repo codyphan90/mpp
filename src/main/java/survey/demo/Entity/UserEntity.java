f9df9dd573a8fdedab15c0aa054aae3a1e03733c
banglekim <banglekim.hp@gmail.com>
2019-03-03 20:20:04 -0600

    Commit UserEntity class

--- src/main/java/survey/demo/Entity/UserEntity.java
+++ src/main/java/survey/demo/Entity/UserEntity.java
@@ -1,5 +1,37 @@
 package survey.demo.Entity;
 
 public class UserEntity {
+	private int id;
+	private String username;
+	private String password;
+	private boolean isAdmin;
+	
+	public Integer getId() {
+        return id;
+    }
 
+    public String getUserName() {
+        return this.username;
+    }
+
+
+    public void setUserName(String userName) {
+        this.username = userName;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    public boolean getIsAdmin () {
+    	return this.isAdmin;
+    }
+    
+    public void setIsAdmin (boolean isAdmin) {
+    	this.isAdmin=isAdmin;
+    }
 }
