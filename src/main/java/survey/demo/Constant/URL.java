8e46af7a141ca3fc1d87e7c495a1e5754cd4f957
banglekim <banglekim.hp@gmail.com>
2019-03-04 00:46:50 -0600

    add and update constants

--- src/main/java/survey/demo/Constant/URL.java
+++ src/main/java/survey/demo/Constant/URL.java
@@ -3,4 +3,5 @@ package survey.demo.Constant;
 public class URL {
     public static final String SURVEY_URL = "/survey";
     public static final String ID = "/{id}";
+    public static final String USER_BASE_URL = "/user";
 }
