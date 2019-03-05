2c9b73415411309a82b37018d9fe340d156393bb
banglekim <banglekim.hp@gmail.com>
2019-03-04 22:26:59 -0600

    Fix typos

--- src/main/java/survey/demo/Controller/UserController.java
+++ src/main/java/survey/demo/Controller/UserController.java
@@ -1,5 +1,55 @@
 package survey.demo.Controller;
+import survey.demo.Response.*;
+import survey.demo.Service.UserService;
+import survey.demo.Entity.*;
+import survey.demo.Service.*;
+import survey.demo.Constant.*;
+import survey.demo.Request.*;
+import javax.persistence.*;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin
+@RestController
+@RequestMapping(value = URL.USER_BASE_URL)
 public class UserController {
-
+	 @Autowired
+	private UserService userService;
+	
+	@RequestMapping(method = RequestMethod.POST)
+    public @ResponseBody ResponseEntity createUser(@RequestBody UserEntity userEntity) {
+		try {
+			userEntity = userService.createUser(userEntity);
+            return new ResponseEntity<>(MessageConstant.CREATE_SUCCESS.replaceAll("@@name@@", userEntity.getUserName()));
+		} catch (Exception e) {
+			return new ResponseEntity<> (false,e.getMessage());
+		}
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+    public @ResponseBody ResponseEntity registerUser(@RequestBody UserEntity userEntity) {
+		try {
+			userEntity = userService.createUser(userEntity);
+            return new ResponseEntity<>(MessageConstant.CREATE_SUCCESS.replaceAll("@@name@@", userEntity.getUserName()));
+		} catch (Exception e) {
+			return new ResponseEntity<> (false,e.getMessage());
+		} 
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+    public @ResponseBody ResponseEntity<String> login(@RequestBody LoginRequest loginRequest) {
+		try {
+			String exceptionMessage = userService.login(loginRequest);
+			if (exceptionMessage != null) {
+				return new ResponseEntity<>(false, exceptionMessage);
+			} else {
+			       return new ResponseEntity<>(MessageConstant.CREATE_SUCCESS.replaceAll("@@name@@", loginRequest.getUserName()));
+			}
+		} catch (Exception e) {
+			return new ResponseEntity<> (false,e.getMessage());
+		}
+	}
+    
+	
 }
--- src/main/java/survey/demo/Service/UserService.java
+++ src/main/java/survey/demo/Service/UserService.java
@@ -1,5 +1,28 @@
 package survey.demo.Service;
 
-public class UserService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 
+import survey.demo.Entity.UserEntity;
+import survey.demo.Repository.UserRepository;
+import survey.demo.Request.*;
+import survey.demo.Constant.*;
+
+public class UserService {
+	@Autowired
+    protected UserRepository usersRepository;
+	
+	public UserEntity createUser(UserEntity userEntity) {
+		userEntity.setPassword(userEntity.getPassword());
+        return usersRepository.save(userEntity);
+	}
+	
+	public String login (LoginRequest loginRequest) {
+		if (StringUtils.isEmpty(loginRequest.getUserName())) return MessageConstant.USER_NAME_OR_PASSWORD_IS_BLANK;
+        if (StringUtils.isEmpty(loginRequest.getPassword())) return MessageConstant.USER_NAME_OR_PASSWORD_IS_BLANK;
+        UserEntity userEntity = usersRepository.findByUserNameEquals(loginRequest.getUserName());
+        if (userEntity == null) return MessageConstant.USER_NAME_OR_PASSWORD_IS_INVALID;
+        if (!loginRequest.getPassword().equals(userEntity.getPassword())) return MessageConstant.USER_NAME_OR_PASSWORD_IS_INVALID;
+        return null;
+	}
 }
