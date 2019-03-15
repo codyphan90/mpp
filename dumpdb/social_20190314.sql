0534af14d2bb737e580785233c47312fbe15ae57
Sandy Nguyen <sandy.wd4@gmail.com>
2019-03-14 21:54:22 -0500

    time zone config

--- dumpdb/social_20190314.sql
+++ dumpdb/social_20190314.sql
@@ -3,7 +3,7 @@
 -- Host: 127.0.0.1    Database: social
 -- ------------------------------------------------------
 -- Server version	8.0.15
-
+use social;
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
@@ -14,6 +14,8 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*SET GLOBAL time_zone = '-5:00';*/
+
 
 --
 -- Table structure for table `comment`
