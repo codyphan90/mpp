aedc76f948f5c529da9d635f822936635db4369d
Phan Viet Anh <phanvietanh90@gmail.com>
2019-03-15 23:14:13 -0500

    update db and lambda library

--- dumpdb/social20190315.sql
+++ dumpdb/social20190315.sql
@@ -33,7 +33,7 @@ CREATE TABLE `comment` (
   PRIMARY KEY (`comment_id`),
   KEY `FKs1slvnkuemjsq2kj4h3vhx7i1` (`post_id`),
   CONSTRAINT `FKs1slvnkuemjsq2kj4h3vhx7i1` FOREIGN KEY (`post_id`) REFERENCES `post` (`post_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -42,7 +42,7 @@ CREATE TABLE `comment` (
 
 LOCK TABLES `comment` WRITE;
 /*!40000 ALTER TABLE `comment` DISABLE KEYS */;
-INSERT INTO `comment` VALUES (1,'This is comment 1',1,'admin',NULL),(2,'This is comment 2',1,'admin',NULL);
+INSERT INTO `comment` VALUES (1,'This is comment 1',1,'admin',NULL),(2,'This is comment 2',1,'admin',NULL),(3,'this is comment of viet anh',2,'vietanh',NULL);
 /*!40000 ALTER TABLE `comment` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -81,8 +81,9 @@ DROP TABLE IF EXISTS `friend_ship`;
  SET character_set_client = utf8mb4 ;
 CREATE TABLE `friend_ship` (
   `id` int(11) NOT NULL,
-  `flow_user_id` int(11) DEFAULT NULL,
-  `user_id` int(11) DEFAULT NULL,
+  `user_name` varchar(50) DEFAULT NULL,
+  `status` varchar(45) DEFAULT NULL,
+  `relate_user_name` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -93,6 +94,7 @@ CREATE TABLE `friend_ship` (
 
 LOCK TABLES `friend_ship` WRITE;
 /*!40000 ALTER TABLE `friend_ship` DISABLE KEYS */;
+INSERT INTO `friend_ship` VALUES (1,'admin','following','vietanh'),(2,'admin','friend','bang'),(3,'admin','pending','sang'),(4,'bang','friend','admin');
 /*!40000 ALTER TABLE `friend_ship` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -106,10 +108,10 @@ DROP TABLE IF EXISTS `post`;
 CREATE TABLE `post` (
   `post_id` int(11) NOT NULL AUTO_INCREMENT,
   `content` text,
-  `created_date` datetime DEFAULT NULL,
   `user_name` varchar(50) DEFAULT NULL,
+  `created_date` datetime DEFAULT NULL,
   PRIMARY KEY (`post_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -118,7 +120,7 @@ CREATE TABLE `post` (
 
 LOCK TABLES `post` WRITE;
 /*!40000 ALTER TABLE `post` DISABLE KEYS */;
-INSERT INTO `post` VALUES (1,'This is post 1',NULL,'admin');
+INSERT INTO `post` VALUES (1,'This is post 1','admin',NULL),(2,'this post of vietanh','vietanh',NULL),(3,'this post of admin','admin',NULL),(4,'this is post of Bang','bang',NULL),(5,'this is post of sang','sang',NULL);
 /*!40000 ALTER TABLE `post` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -134,10 +136,9 @@ CREATE TABLE `user` (
   `user_name` varchar(45) DEFAULT NULL,
   `password` varchar(100) DEFAULT NULL,
   `full_name` varchar(45) DEFAULT NULL,
-  `is_admin` bit(1) DEFAULT NULL,
   PRIMARY KEY (`user_id`),
   UNIQUE KEY `user_name_UNIQUE` (`user_name`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -146,7 +147,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'admin','12345',NULL,NULL);
+INSERT INTO `user` VALUES (1,'admin','12345',NULL),(2,'vietanh','12345',NULL),(3,'bang','12345',NULL),(4,'sang','12345',NULL);
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -159,4 +160,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-03-15 20:41:13
+-- Dump completed on 2019-03-15 23:13:44
---
+++ src/main/java/demo/mpp/utils/LambdaLibrary.java
@@ -0,0 +1,4 @@
+package demo.mpp.utils;
+
+public class LambdaLibrary {
+}
