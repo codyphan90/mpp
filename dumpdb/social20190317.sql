2d035208bc432edef11235e789d38e97858153f2
Phan Viet Anh <phanvietanh90@gmail.com>
2019-03-17 14:41:25 -0500

    update db

---
+++ dumpdb/social20190317.sql
@@ -0,0 +1,164 @@
+CREATE DATABASE  IF NOT EXISTS `social` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
+USE `social`;
+-- MySQL dump 10.13  Distrib 8.0.15, for macos10.14 (x86_64)
+--
+-- Host: 127.0.0.1    Database: social
+-- ------------------------------------------------------
+-- Server version	8.0.15
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+ SET NAMES utf8 ;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `comment`
+--
+
+DROP TABLE IF EXISTS `comment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `comment` (
+  `comment_id` int(11) NOT NULL AUTO_INCREMENT,
+  `content` text,
+  `post_id` int(11) DEFAULT NULL,
+  `user_name` varchar(250) DEFAULT NULL,
+  `created_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`comment_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `comment`
+--
+
+LOCK TABLES `comment` WRITE;
+/*!40000 ALTER TABLE `comment` DISABLE KEYS */;
+INSERT INTO `comment` VALUES (4,'this comment to Post of Tam',6,'vietanh','2019-03-17 14:59:18');
+/*!40000 ALTER TABLE `comment` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `emotion`
+--
+
+DROP TABLE IF EXISTS `emotion`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `emotion` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `post_id` int(11) DEFAULT NULL,
+  `user_name` varchar(50) DEFAULT NULL,
+  `type` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `emotion`
+--
+
+LOCK TABLES `emotion` WRITE;
+/*!40000 ALTER TABLE `emotion` DISABLE KEYS */;
+INSERT INTO `emotion` VALUES (4,6,'vietanh','like');
+/*!40000 ALTER TABLE `emotion` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `friend_ship`
+--
+
+DROP TABLE IF EXISTS `friend_ship`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `friend_ship` (
+  `id` int(11) NOT NULL,
+  `user_name` varchar(50) DEFAULT NULL,
+  `relate_user_name` varchar(50) DEFAULT NULL,
+  `following` tinyint(1) DEFAULT NULL,
+  `friend` tinyint(1) DEFAULT NULL,
+  `status` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `friend_ship`
+--
+
+LOCK TABLES `friend_ship` WRITE;
+/*!40000 ALTER TABLE `friend_ship` DISABLE KEYS */;
+INSERT INTO `friend_ship` VALUES (1,'admin','vietanh',1,1,'sucess'),(2,'admin','bang',1,NULL,NULL),(3,'admin','sang',1,NULL,'pending'),(4,'admin','huy',0,1,'success'),(5,'admin','tam',1,1,'sucess');
+/*!40000 ALTER TABLE `friend_ship` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `post`
+--
+
+DROP TABLE IF EXISTS `post`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `post` (
+  `post_id` int(11) NOT NULL AUTO_INCREMENT,
+  `content` text,
+  `user_name` varchar(50) DEFAULT NULL,
+  `created_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`post_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `post`
+--
+
+LOCK TABLES `post` WRITE;
+/*!40000 ALTER TABLE `post` DISABLE KEYS */;
+INSERT INTO `post` VALUES (6,'this post of tam','tam','2019-03-17 14:58:58'),(8,'this post 2 of vietanh','vietanh','2019-03-16 22:36:51'),(9,'this post 3 of vietanh','vietanh','2019-03-16 22:43:15'),(10,'this post 4 of vietanh','vietanh','2019-03-16 22:45:08'),(11,'this post 4 of vietanh','vietanh','2019-03-16 23:08:52'),(12,'this post of bang','bang','2019-03-16 23:11:24'),(13,'this post of sang','sang','2019-03-16 23:12:03'),(14,'this post of huy','huy','2019-03-16 23:12:24'),(15,'this post of tam','tam','2019-03-16 23:12:33');
+/*!40000 ALTER TABLE `post` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `user` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(45) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  `full_name` varchar(45) DEFAULT NULL,
+  `dob` date DEFAULT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `user_name_UNIQUE` (`user_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'admin','12345',NULL,NULL),(2,'vietanh','12345',NULL,NULL),(3,'bang','12345',NULL,NULL),(4,'sang','12345',NULL,NULL),(5,'huy','12345',NULL,NULL),(6,'tam','12345',NULL,NULL);
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-03-17 14:40:44
