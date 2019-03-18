9ea6c843615f8a51886f49592810320c0925ff59
banglekim <banglekim.hp@gmail.com>
2019-03-18 00:14:46 -0500

    made functions as final

--- src/main/java/demo/mpp/utils/FriendshipFunctions.java
+++ src/main/java/demo/mpp/utils/FriendshipFunctions.java
@@ -11,12 +11,12 @@ import java.util.stream.Collectors;
 public class FriendshipFunctions {
 
     //Convert from list of user entity to list of user name
-    public static Function<List<UserEntity>,List<String>> convertUserEntity2UserName = (userList) ->userList.stream()
+    public static final Function<List<UserEntity>,List<String>> convertUserEntity2UserName = (userList) ->userList.stream()
             .map(user->user.getUserName())
             .collect(Collectors.toList());
 
     // Collect list of users who does not have friendship
-    public static BiFunction<List<UserEntity>,List<FriendShipEntity>,List<UserEntity>> collectNotFriendShip =(listUser, listFriendship)-> listUser.stream()
+    public static final BiFunction<List<UserEntity>,List<FriendShipEntity>,List<UserEntity>> collectNotFriendShip =(listUser, listFriendship)-> listUser.stream()
             .filter(user->!listFriendship.stream()
                     .map(frs->frs.getUserName())
                     .collect(Collectors.toList())
@@ -24,7 +24,7 @@ public class FriendshipFunctions {
             .collect(Collectors.toList());
 
     // From list of Friendship from db, filter friends/followings of 1 user
-    public static BiFunction<List<FriendShipEntity>, String, List<FriendShipEntity>> collectUserFriendship = (userFrsList,targetUserName) -> userFrsList
+    public static final BiFunction<List<FriendShipEntity>, String, List<FriendShipEntity>> collectUserFriendship = (userFrsList,targetUserName) -> userFrsList
             .stream()
             .filter(frs -> frs.getUserName().equals(targetUserName))
             .collect(Collectors.toList());
